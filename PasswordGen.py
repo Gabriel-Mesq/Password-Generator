52b5a32e1c23c6be03f07eff52baff51208fd414
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2022-10-27 00:52:20 -0300

    Update PasswordGen.py

--- PasswordGen.py
+++ PasswordGen.py
@@ -7,7 +7,7 @@ upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '
 nmbrs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symb  = ['!', '?', '@', '#', '$', '%', '&', '*', '-', '+', '=', '(', ')', '[', ']'] 
 
-all = lower + upper + symb
+all = lower + upper + nmbrs + symb
 random.shuffle(all)
 
 password = []
